0c6063360f55ca5b99301432e83cfe27df5a2feb
Youngwan <tiny657@gmail.com>
2025-12-08 14:45:07 -0800

    fix

--- trainers/rewards/__init__.py
+++ trainers/rewards/__init__.py
@@ -1,14 +1,17 @@
 """Reward functions for reinforcement learning training."""
-from .accuracy_rewards import exact_match_reward_func, digit_reward_func
-from .format_rewards import structured_xml_reward_func
-from .custom_rewards import custom_reward_func
-from .subjective_rewards import subjective_api_reward_func_simple
 
 
 def reward_function(func):
     """Decorator to mark functions as reward functions."""
     func._is_reward_function = True
     return func
+
+
+# Import after defining reward_function to avoid circular import
+from .accuracy_rewards import exact_match_reward_func, digit_reward_func
+from .format_rewards import structured_xml_reward_func
+from .custom_rewards import custom_reward_func
+from .subjective_rewards import subjective_api_reward_func_simple
 
 
 __all__ = [
